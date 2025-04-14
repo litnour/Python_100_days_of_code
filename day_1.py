e1e543c2b5ccbdaf6c13486cbbbe021f5c83d569
litnour <nour.litzler@icloud.com>
2025-04-14 08:16:50 +0200

    day 1 of 100 days of code add

---
+++ day_1.py
@@ -0,0 +1,6 @@
+### Day 1 -- brand name generator ###
+
+print("Welcome to the Brand Name Generator")
+city = input("Which city did you grow up in?")
+pet = input("What is the name of a pet?")
+print("Your band name could be: " + city + " " + pet)
